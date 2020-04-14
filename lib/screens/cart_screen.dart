37cc47e41a0ccaab3d2f4be54d97add46d984756
MilanDavid <miland.sm@gmail.com>
2020-04-14 12:14:28 +0200

    some more styling

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -20,13 +20,24 @@ class CartScreen extends StatelessWidget {
             child: Padding(
               padding: EdgeInsets.all(8.0),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Text('Total', style: TextStyle(fontSize: 20.0)),
-                  SizedBox(width: 10.0),
+                  Spacer(),
                   Chip(
-                    label: Text('\$${cart.totalAmount}'),
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                          color:
+                              Theme.of(context).primaryTextTheme.title.color),
+                    ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
+                  FlatButton(
+                    child: Text('ORDER NOW'),
+                    onPressed: () {},
+                    textColor: Theme.of(context).primaryColor,
+                  )
                 ],
               ),
             ),
